ff973714100b64917a45ac977c1898d78ecdc9a0
malik-evans <malikevanstab393@gmail.com>
2019-07-06 12:22:38 -0400

    import CoreModule into AppModule

--- src/Scaledriven.App/projects/core/src/lib/core.module.ts
+++ src/Scaledriven.App/projects/core/src/lib/core.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CoreComponent } from './core.component';
+import { NgxsModule } from "@ngxs/store";
+import { NgxsDispatchPluginModule } from "@ngxs-labs/dispatch-decorator";
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { NgxsEmitPluginModule } from "@ngxs-labs/emitter";
+import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
 
 @NgModule({
   declarations: [CoreComponent],
   imports: [
+    NgxsModule.forRoot([]),
+    NgxsDispatchPluginModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsEmitPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot()
   ],
-  exports: [CoreComponent]
+  exports: [
+    CoreComponent,
+    NgxsModule,
+    NgxsDispatchPluginModule,
+    NgxsReduxDevtoolsPluginModule,
+    NgxsEmitPluginModule,
+    NgxsLoggerPluginModule
+  ]
 })
 export class CoreModule { }

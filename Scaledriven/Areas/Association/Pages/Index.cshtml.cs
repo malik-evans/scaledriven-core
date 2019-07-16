f651f9e95b3f5224c013b7463ca2d78a3de6695f
malik-evans <malikevanstab393@gmail.com>
2019-07-16 18:43:37 -0400

    build(Scaledriven): application page layer

---
+++ Scaledriven/Areas/Association/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Scaledriven.Areas.Association.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Scaledriven/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Scaledriven.Pages
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
